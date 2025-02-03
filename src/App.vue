f4b9dfca99b13ee42012dc14d65d2557cc737c37
leonardo Cueto <leocueto1999@gmail.com>
2025-02-03 15:46:50 -0300

    initialize Vue 3 project with Vite, Tailwind CSS, and ESLint configuration

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <component :is="DefaultLayout">
+    <router-view />
+  </component>
+</template>
+<script setup lang="ts">
+import DefaultLayout from '@/layouts/DefaultLayout.vue'
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<template>
+  <main class="bg-blue-200 w-xl p-4">
+    <h1 class="font-bold text-2xl">
+      {{ title }}
+    </h1>
+  </main>
+</template>
+<script setup lang="ts">
+const title = "What Rick and Morty Character's are you looking for?"
+</script>
