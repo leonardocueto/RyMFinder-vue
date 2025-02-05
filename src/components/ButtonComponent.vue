876531757c6ee5c863efe6ecc4cd510da3393a46
leonardo Cueto <leocueto1999@gmail.com>
2025-02-05 13:37:13 -0300

    add ButtonComponent and IconComponent for enhanced UI functionality

---
+++ src/components/ButtonComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <button
+    type="submit"
+    class="absolute left-0 text-black p-2 rounded-l-full hover:bg-gray-200 cursor-pointer"
+    :disabled="props.disabled"
+  >
+    <div v-if="loading" class="animate-spin w-5">
+      <icon-component icon="IconLoader2" size="small" />
+    </div>
+    <slot v-else />
+  </button>
+</template>
+<script lang="ts" setup>
+const props = withDefaults(
+  defineProps<{
+    loading?: boolean
+    disabled?: boolean
+  }>(),
+  {
+    loading: false,
+    disabled: false,
+  },
+)
+
+import IconComponent from '@/components/IconComponent.vue'
+</script>
---
+++ src/components/IconComponent.vue
@@ -0,0 +1,35 @@
+<template>
+  <component :is="iconComponent" :key="iconComponent" :size="size" :color="color" />
+</template>
+<script setup lang="ts">
+import TablerIcons from '@/assets/icons'
+import { computed } from 'vue'
+
+const props = withDefaults(
+  defineProps<{
+    icon: string
+    size?: 'small' | 'medium' | 'large'
+    color?: string
+  }>(),
+  {
+    icon: '',
+    size: 'medium',
+    color: 'gray',
+  },
+)
+
+const iconComponent = computed(() => TablerIcons[props.icon as keyof typeof TablerIcons])
+
+const size = computed(() => {
+  switch (props.size) {
+    case 'small':
+      return 20
+    case 'medium':
+      return 25
+    case 'large':
+      return 40
+    default:
+      return 30
+  }
+})
+</script>
