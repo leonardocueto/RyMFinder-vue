5aef5f638625e7a41c31f8fdf8635d26f7bad33c
leonardo Cueto <leocueto1999@gmail.com>
2025-02-05 13:38:34 -0300

    add CategoryComponent and CategoryOptionComponent for improved category navigation

---
+++ src/components/CategoryComponent.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex flex-col md:flex-row gap-4 w-full">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/CategoryOptionComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <router-link
+    :to="{ name: props.link }"
+    class="flex justify-between items-center bg-red-400 rounded-xl text-white px-2 w-full shadow-md shadow-red-400 hover:shadow-none cursor-pointer transition-all ease-in duration-300 hover:bg-red-500"
+  >
+    <p class="m-4 md:m-7">{{ props.text }}</p>
+  </router-link>
+</template>
+<script lang="ts" setup>
+const props = defineProps<{
+  text: string
+  link: string
+}>()
+</script>
