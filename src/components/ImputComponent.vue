cb869f1669c98d2d984952679bcc31166d427866
leonardo Cueto <leocueto1999@gmail.com>
2025-02-05 13:37:58 -0300

    add InputComponent and SearchComponent for user input handling

---
+++ src/components/ImputComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <input
+    type="text"
+    class="w-full py-2 pl-15 bg-gray-100 rounded-3xl transition-all duration-200 focus:outline-none focus:ring-2 focus:ring-gray-500"
+    :value="modelValue"
+    @input="updateValue"
+    :disabled="props.disabled"
+    :placeholder="props.placeholder"
+  />
+</template>
+<script lang="ts" setup>
+const props = withDefaults(
+  defineProps<{
+    modelValue: string
+    disabled: boolean
+    placeholder: string
+  }>(),
+  {
+    modelValue: '',
+    disabled: false,
+    placeholder: '',
+  },
+)
+
+const emit = defineEmits<{
+  (event: 'update:modelValue', value: string): void
+}>()
+function updateValue(event: Event) {
+  const input = event.target as HTMLInputElement
+  emit('update:modelValue', input.value)
+}
+</script>
---
+++ src/components/SearchComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <form @submit.prevent="handleSubmit" class="relative">
+    <input-component
+      v-model="text"
+      :disabled="loading"
+      placeholder="Search Character, location, episode, etc."
+    />
+    <button-component :loading="loading" :disabled="loading">
+      <icon-component icon="IconSearch" size="medium" />
+    </button-component>
+  </form>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue'
+import InputComponent from '@/components/ImputComponent.vue'
+import ButtonComponent from '@/components/ButtonComponent.vue'
+import IconComponent from '@/components/IconComponent.vue'
+const text = ref('')
+const loading = ref(false)
+const handleSubmit = () => {
+  if (text.value.length > 0) {
+    loading.value = true
+    setTimeout(() => {
+      console.log(text.value.trim())
+
+      text.value = ''
+      loading.value = false
+    }, 1000)
+  }
+}
+</script>
