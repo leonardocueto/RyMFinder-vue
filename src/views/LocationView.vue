9aba17db5ebfaa4363cb83deb5e1b94da7b85a27
leonardo Cueto <leocueto1999@gmail.com>
2025-02-06 17:50:24 -0300

    enhance LocationView; implement infinite scroll for location loading and refactor to use BaseViewComponent

--- src/views/LocationView.vue
+++ src/views/LocationView.vue
@@ -1,13 +1,30 @@
 <template>
-  <header class="flex items-center justify-between">
-    <router-link to="/" class="p-2 cursor-pointer hover:bg-gray-200 rounded-full">
-      <icon-component icon="IconArrowLeft" size="medium" />
-    </router-link>
-    <h1 class="font-bold text-xl md:text-2xl text-center">Location</h1>
-  </header>
-  <list-cards-component> </list-cards-component>
+  <base-view-component title="Location" :data="locations" :loading="loading" type="location" />
 </template>
 <script lang="ts" setup>
-import IconComponent from '@/components/IconComponent.vue'
-import ListCardsComponent from '@/components/ListCardsComponent.vue'
+import { computed, onMounted, onUnmounted } from 'vue'
+
+import BaseViewComponent from '@/components/BaseViewComponent.vue'
+import useLocation from '@/composables/useLocation'
+
+const { getLocation, listLocations, loading } = useLocation()
+
+const locations = computed(() => listLocations.value)
+
+const handleScroll = async () => {
+  const { scrollHeight, scrollTop, clientHeight } = document.documentElement
+
+  if (scrollTop + clientHeight >= scrollHeight - 150 && !loading.value) {
+    await getLocation()
+  }
+}
+
+onMounted(async () => {
+  await getLocation()
+  window.addEventListener('scroll', handleScroll)
+})
+
+onUnmounted(() => {
+  window.removeEventListener('scroll', handleScroll)
+})
 </script>
