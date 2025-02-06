d4c16244d4a9abcaa5b6e42736e39a06e907762d
leonardo Cueto <leocueto1999@gmail.com>
2025-02-06 17:50:01 -0300

    add EpisodeDetail component; implement episode loading logic with route parameter handling

---
+++ src/views/EpisodeDetail.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>1</div>
+</template>
+<script lang="ts" setup>
+import { ref, onMounted } from 'vue'
+
+import { useRoute } from 'vue-router'
+
+import useEpisode from '@/composables/useEpisode'
+
+const route = useRoute()
+const id = route.params.id
+const isLoading = ref(true)
+
+const { filterEpisode } = useEpisode()
+
+onMounted(async () => {
+  isLoading.value = true
+  const result = await filterEpisode(id.toString())
+  console.log(result)
+  isLoading.value = false
+})
+</script>
