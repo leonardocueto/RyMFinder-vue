26d3c56ee48f18a7a256f3ef4691f6a2ed245548
leonardo Cueto <leocueto1999@gmail.com>
2025-02-06 17:50:20 -0300

    add LocationDetail component; implement location loading logic with route parameter handling

---
+++ src/views/LocationDetail.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>1</div>
+</template>
+<script lang="ts" setup>
+import { ref, onMounted } from 'vue'
+
+import { useRoute } from 'vue-router'
+
+import useLocation from '@/composables/useLocation'
+
+const route = useRoute()
+const id = route.params.id
+const isLoading = ref(true)
+
+const { filterLocation } = useLocation()
+
+onMounted(async () => {
+  isLoading.value = true
+  const result = await filterLocation(id.toString())
+  console.log(result)
+  isLoading.value = false
+})
+</script>
