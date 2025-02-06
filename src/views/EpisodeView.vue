5a8d9d434e96467b512be64f110aa9ab1318f6af
leonardo Cueto <leocueto1999@gmail.com>
2025-02-06 17:50:07 -0300

    add EpisodeView component; implement episode loading with infinite scroll and enhance data handling

--- src/views/EpisodeView.vue
+++ src/views/EpisodeView.vue
@@ -1,3 +1,29 @@
 <template>
-  <p>episode</p>
+  <base-view-component title="Episode" :data="episodes" :loading="loading" type="episode" />
 </template>
+<script lang="ts" setup>
+import { computed, onMounted, onUnmounted } from 'vue'
+
+import BaseViewComponent from '@/components/BaseViewComponent.vue'
+import useEpisode from '@/composables/useEpisode'
+
+const { getEpisode, listEpisodes, loading } = useEpisode()
+
+const episodes = computed(() => listEpisodes.value)
+
+const handleScroll = async () => {
+  const { scrollHeight, clientHeight, scrollTop } = document.documentElement
+  if (scrollTop + clientHeight >= scrollHeight - 150 && !loading.value) {
+    await getEpisode()
+  }
+}
+
+onMounted(async () => {
+  await getEpisode()
+  window.addEventListener('scroll', handleScroll)
+})
+
+onUnmounted(async () => {
+  window.removeEventListener('scroll', handleScroll)
+})
+</script>
